922743b944deeadcb108624f05b17f3ad05e1ec9
Isaiah Myles <99617402+Expert21@users.noreply.github.com>
2025-11-23 22:19:07 -0600

    Fixed init file

---
+++ src/__init__.py
@@ -0,0 +1,20 @@
+"""
+Hermes OSINT Tool
+Advanced Open Source Intelligence Gathering Framework
+"""
+
+__version__ = "1.3.2"
+__author__ = "Expert21"
+__description__ = "Advanced OSINT Intelligence Gathering Tool"
+
+# Core modules
+from src.core.logger import setup_logger
+from src.core.config import load_config
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__description__",
+    "setup_logger",
+    "load_config",
+]
